996fc16f593edbed623190e8d8790ce5f5dc0199
mohamed <60613498+mohamedgomycode@users.noreply.github.com>
2020-04-06 12:32:56 +0200

    Add files via upload

---
+++ src/component/Description.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Description extends Component {
+  render() {
+    if (!this.props.movies) {
+      return <p>there's no movie</p>;
+    }
+    const [movies] = this.props.movies.filter(
+      el => el.id === this.props.match.params.id
+    );
+    console.log(movies);
+    return (
+      <div>
+        <h1>DESCRIPTION PAGE</h1>
+        <h1>{movies.discription}</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  movies: state.movieReducer.movies
+});
+
+export default connect(mapStateToProps)(Description);
